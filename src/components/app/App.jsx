873b3283f1e0ec43a8e108deed7a752680f722f2
David Delgadillo <daviddel.731@gmail.com>
2021-08-16 10:31:43 -0700

    Added light/dark mode on header and successfully changing document colors

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CharacterList from '../characters/CharacterList';
 import CharacterToggle from '../characters/CharacterToggle';
+import Header from '../header/Header';
 
 export default function App() {
   return (
     <>
+      <Header />
       <CharacterToggle />
       <CharacterList />
     </>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTheme, useToggle } from '../../state/ThemeProvider';
+import styled from 'styled-components';
 
 function Header() {
+  const theme  = useTheme();
+  const toggleTheme = useToggle();
+
+  
   return (
-    <div>
-      
-    </div>
+    <HeaderStyled>
+      <h1>Header</h1>
+      <button onClick={toggleTheme}>Light/Dark Mode</button>
+      {theme}
+    </HeaderStyled>
   );
 }
 
 export default Header;
+
+const HeaderStyled = styled.header`
+  width: 100vw;
+  height: 80px;
+  background-color: lightblue;
+`;
